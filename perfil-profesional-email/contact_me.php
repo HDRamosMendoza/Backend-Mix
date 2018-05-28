bbb9968bcd3991bad10e42933568183d16b222ec
Daniel Ramos <heber.daniel.ramos.mendoza@gmail.com>
2018-05-28 09:57:36 -0500

    Prueba 3 envio de correo

--- perfil-profesional-email/contact_me.php
+++ perfil-profesional-email/contact_me.php
@@ -10,10 +10,20 @@
    return false;
    }
  */  
+/*
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+*/
+
+$name = "name";
+$email_address = "email@gmail.com";
+$phone = "999130638";
+$message = "menssage";
+
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
    
 // Create the email and send the message
 $to = 'heber.daniel.ramos.mendoza@gmail.com'; 
@@ -21,7 +31,7 @@ $to = 'heber.daniel.ramos.mendoza@gmail.com';
 $email_subject = "Website Contact Form: ";
 $email_body = "Prueba"; 
 // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: ";   
+$headers .= "From: Geeky Theory heber.ramos.mendoza@gmail.com \r\n";   
 
 mail($to,$email_subject,$email_body,$headers);
 return true;         
