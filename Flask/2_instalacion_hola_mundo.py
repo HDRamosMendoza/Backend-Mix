1a57505a3d23ab693d148dbb3b7292204a6a0c8f
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2018-07-22 11:51:14 -0500

    Instalación y hola mundo

--- Flask/2_instalacion_hola_mundo.py
+++ Flask/2_instalacion_hola_mundo.py
@@ -1,4 +1,13 @@
 from flask import Flask
+#http://flask.pocoo.org/docs/1.0/installation/#installation
 
+# Nuevo objeto
 app = Flask(__name__)
+
+# Decorador 
+@app.route('/')
+def inde():
+	return 'Hola Mundo'
+
+# Ejecuta el servidor 
 app.run()
