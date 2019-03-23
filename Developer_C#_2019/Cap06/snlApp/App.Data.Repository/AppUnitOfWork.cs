e8313fdcd378954983e6f193a764f698ecca0593
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-03-23 17:09:13 -0500

    UnitOfWork - parte 1
    Primera parte del curso. Tema UNITOFWORK.

---
+++ Developer_C#_2019/Cap06/snlApp/App.Data.Repository/AppUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using App.Data.DataAccess;
+using App.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Repository
+{
+    // Constructpr: Si no se requiere comportamioento.
+    // new crea una instancia de la clase.
+    //DB dATAMODEL. Se encuentra las instancia
+    // USING. 
+    public class AppUnitOfWork : IAppUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public IArtistRepository ArtistRepository { get ; set; }
+        public ICustomerRepository CustomerRepository { get; set; }
+        
+        // Ni creao el AppUnitOfWork creo la instancia 
+        public AppUnitOfWork()
+        {
+            _context = new DBDataModel();
+
+            this.ArtistRepository = new ArtistRepository(_context);
+            this.CustomerRepository = new CustomerRepository(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Developer_C#_2019/Cap06/snlApp/App.Data.Repository/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using App.Data.Repository.Interface;
+using App.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Repository
+{
+    public class GenericRepository<TEntity>:IGenericRepository<TEntity>
+        where TEntity:class
+    {
+        protected readonly DbContext _context;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            //En el unitOfWork ya lo tiene implementado.
+            //_context.SaveChanges();
+            return true;
+        }
+
+        //public bool Add(TEntity entity)
+        //{
+        //    _context.Set<TEntity>().Add(entity);
+        //    //Confirmando los cambios
+        //    _context.SaveChanges();
+        //    return true;
+        //}
+
+        public int Count()
+        {
+           return _context.Set<TEntity>().Count();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById<TId>(TId id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Attach(entity);
+            _context.Set<TEntity>().Remove(entity);
+            //_context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Attach(entity);
+            _context.Entry(entity).State=EntityState.Modified;
+            //_context.SaveChanges();
+            return true;
+        }
+    }
+}
