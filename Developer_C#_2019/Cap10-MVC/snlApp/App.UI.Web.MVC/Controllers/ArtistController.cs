7bff2b639c6b4fe0dbbf9a6019ea1c143c7d99c3
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-05-04 20:04:37 -0500

    Clase 11 - Parte 1
    Clase parte 1. De la clase 11

---
+++ Developer_C#_2019/Cap10-MVC/snlApp/App.UI.Web.MVC/Controllers/ArtistController.cs
@@ -0,0 +1,42 @@
+﻿using App.UI.Web.MVC.MantenimientosService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.UI.Web.MVC.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly MantenimientosServiceClient mantenimientosServiceClient;
+
+        public ArtistController() {
+            mantenimientosServiceClient = new MantenimientosServiceClient();
+
+        }
+
+        // GET: Artist
+        public ActionResult Index(string artistaNombre)
+        {
+            artistaNombre = artistaNombre ?? "";
+            var model = mantenimientosServiceClient.GetArtistAll(artistaNombre);
+            ViewBag.artistaFiltrado = artistaNombre;
+            return View(model);
+        }
+
+        public ActionResult Buscar()
+        {
+            return View();
+        }
+
+        public ActionResult ObtenerArtistas(string artistaNombre)
+        {
+            artistaNombre = artistaNombre ?? "";
+            var listado = mantenimientosServiceClient.GetArtistAll(artistaNombre);
+            ViewBag.artistaFiltrado = artistaNombre;
+
+            return PartialView(listado);
+        }
+    }
+}
