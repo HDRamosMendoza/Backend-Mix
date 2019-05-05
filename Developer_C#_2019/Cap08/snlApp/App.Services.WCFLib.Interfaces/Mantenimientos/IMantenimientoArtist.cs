0ad6f67787602a1b3e2dd24feb61c8b0cb2f755b
H. Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-05-04 20:59:19 -0500

    Clase 11 - parte 3
    Ultima parte adicional

---
+++ Developer_C#_2019/Cap08/snlApp/App.Services.WCFLib.Interfaces/Mantenimientos/IMantenimientoArtist.cs
@@ -0,0 +1,27 @@
+﻿using App.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.WCFLib.Interfaces
+{
+    public partial interface IMantenimientosService
+    {
+        [OperationContract]
+        IEnumerable<Artist> GetArtistAll(string nombre);
+        // Definimos el contrato
+
+        [OperationContract]
+        Artist GetArtistById(int id);
+
+        [OperationContract]
+        bool SaveArtist(Artist artist);
+
+        [OperationContract]
+        bool DeleteArtist(int id);
+
+    }
+}
